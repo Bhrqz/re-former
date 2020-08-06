2f01a9c09abe8d7452c632692085e221b8dcbe4e
Bhrqz <josebchrist@hotmail.com>
2020-08-06 11:55:08 -0500

    starting editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+
   end
 
   def create
-      #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_users_path
     else
@@ -13,4 +15,10 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+  
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
 end
