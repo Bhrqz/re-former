21c184a568f3a2a75b227b878c72acadf00bbb27
Bhrqz <josebchrist@hotmail.com>
2020-08-06 09:41:12 -0500

    second commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+      #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
+    end
+  end
+  
+end
